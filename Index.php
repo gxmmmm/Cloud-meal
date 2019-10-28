ee9f1a8b03f0f0166641a20906601cc81a023b33
pmmiiiz <56476217+pmmiiiz@users.noreply.github.com>
2019-10-28 17:43:50 +0700

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+<head>
+<link href="style.css" rel="stylesheet" type="text/css" />
+<meta charset="utf-8">
+<title>Index</title>
+</head>
+<body>
+	<table width="100%" bgcolor="#F3CA81" align="center">
+	<tr>
+		<td height="30"><marquee>Information Technology KMITL</maruqee>
+		</td>
+	</tr>
+	<tr>
+		<td height="150" align="center" bgcolor="#E9DDC0">header
+		</td>
+	</tr>
+	<tr>
+		<td>home
+		</td>
+	</tr>
+	<tr>
+		<td height="500" align="center" bgcolor="#E9DDC0">
+		</td>
+	</tr>
+	</table>
+</body>
+</html>
