ea58d1bf7791e1b6e6a06563ec9a184d79d52caa
boomsocool <56288435+boomsocool@users.noreply.github.com>
2019-11-05 21:46:30 +0700

    sdfsdf
    safsdf

--- Index.php
+++ Index.php
@@ -6,7 +6,7 @@
 <title>Index</title>
 </head>
 <body>
-	<table width="100%" bgcolor="#F3CA81" align="center">
+	<table width="90%" bgcolor="#F3CA81" align="center">
 	<tr>
 		<td height="30"><marquee>Information Technology KMITL</marquee>
 		</td>
@@ -16,11 +16,22 @@
 		</td>
 	</tr>
 	<tr>
-		<td>home
+		<td height="50"><ul>
+  							<li><a href="">Home</a></li>
+  							<li><a href="">News</a></li>
+  							<li><a href="">Contact</a></li>
+  							<li><a href="">About</a></li>
+						</ul>
 		</td>
 	</tr>
 	<tr>
 		<td height="500" align="center" bgcolor="#E9DDC0">
+		<table width="100%" border="1" height="100%">
+  				<tr>
+    				<td width="25%"></td>
+    				<td></td>
+  				</tr>
+			</table>
 		</td>
 	</tr>
 	</table>
