2a1318bfc39baa2081051cf567f7b4aafde4118b
boomsocool <56288435+boomsocool@users.noreply.github.com>
2019-11-25 21:43:14 +0700

    asssssssss
    ss

--- about.php
+++ about.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Photomedia</title>
+    <title>CloudMeal</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -27,270 +27,12 @@
 </head>
 
 <body>
-    <!--[if lte IE 9]>
-            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
-        <![endif]-->
-
-    <!-- header-start -->
     <header>
-        <div class="header-area ">
-            <div class="header_top">
-                <div class="container">
-                    <div class="row align-items-center">
-                        <div class="col-xl-4 col-md-4 d-none d-md-block">
-                            <div class="header_links ">
-                                <ul>
-                                    <li><a href="#"> <i class="fa fa-facebook"></i> </a></li>
-                                    <li><a class="twiter" href="#"> <i class="fa fa-twitter"></i> </a></li>
-                                    <li><a class="insta" href="#"> <i class="fa fa-instagram"></i> </a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="col-xl-4 col-md-4">
-                            <div class="logo">
-                                <a href="index.html">
-                                    <img src="img/logo.png" alt="">
-                                </a>
-                            </div>
-                        </div>
-                        <div class="col-xl-4 col-md-4 d-none d-md-block">
-                            <div class="login_resiter">
-                                <p><a href="#"><i class="flaticon-user"></i>login</a> | <a href="#">Resister</a></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div id="sticky-header" class="main-header-area white-bg">
-                <div class="container">
-                    <div class="row align-items-center">
-                        <div class="col-xl-7 col-lg-7">
-                            <div class="main-menu  d-none d-lg-block">
-                                <? include("menu.php"); ?>
-                            </div>
-                        </div>
-                        <div class="col-xl-5 col-lg-5">
-                            <div class="get_serch">
-                                <a id="search_1" href="javascript:void(0)"><i class="ti-search"></i></a>
-                            </div>
-                        </div>
-                        <div class="col-12">
-                            <div class="mobile_menu d-block d-lg-none"></div>
-                        </div>
-                    </div>
-                    <div class="search_input" id="search_input_box">
-                        <div class="container ">
-                            <form class="d-flex justify-content-between search-inner">
-                                <input type="text" class="form-control" id="search_input" placeholder="Search Here">
-                                <button type="submit" class="btn"></button>
-                                <span class="ti-close" id="close_search" title="Close Search"></span>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+		<? include("menu.php")?>
     </header>
     <!-- header-end -->
 
-    <!-- about_area_start -->
-    <div class="about_area">
-        <div class="container">
-            <div class="row">
-                <div class="col-xl-12">
-                    <div class="about_text">
-                        <h3>Lost Is Just a Four Letter Word</h3>
-                        <p class="about_text_1">For man shall him gathering image dry gathering which cattle. Said
-                            second fruit dry Upon cattle sixth lights herb gathered second that night sea his blessed he
-                            us meat Seed void. Day saw him days itself lights. Give waters multiply earth very brought
-                            replenish open itself multiply upon cattle she’d rule make doesn’t lights together I let
-                            heaven which for land saw grass our was unto face brought seed dry moving was, were great.
-                            Our won’t dry herb set over. Seasons earth upon fill it after. Shall she’d hath so which
-                            seas to </p>
-                        <p class="about_text_2">
-                            For man shall him gathering image dry gathering which cattle. Said second fruit dry Upon
-                            cattle sixth lights herb gathered second that night sea his blessed he us meat Seed void.
-                            Day saw him days itself lights. Give waters multiply earth very brought replenish open
-                            itself multiply upon cattle
-                        </p>
-                    </div>
-                </div>
-                <div class="col-xl-12">
-                    <div class="about_thumb">
-                        <img src="img/about/banner.png" alt="">
-                    </div>
-                </div>
-                <div class="about_details">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-xl-6">
-                                <p>For man shall him gathering image dry gathering which cattle. Said second fruit dry
-                                    Upon cattle sixth lights herb gathered second that night sea his blessed he us meat
-                                    Seed void. Day saw him days itself lights.</p>
-                            </div>
-                            <div class="col-xl-6">
-                                <p>For man shall him gathering image dry gathering which cattle. Said second fruit dry
-                                    Upon cattle sixth lights herb gathered second that night sea his blessed he us meat
-                                    Seed void. Day saw him days itself lights.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- about_area_start -->
 
-    <div class="moderators_area">
-        <div class="container">
-            <div class="modarator_border"></div>
-            <div class="row">
-                <div class="col-xl-12">
-                    <div class="modarator_title">
-                        <h3>Our Moderators</h3>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xl-3 col-md-3">
-                    <div class="single_modarators">
-                        <div class="moderator_thumb">
-                            <img src="img/team/1.png" alt="">
-                            <div class="author_links">
-                                <ul>
-                                    <li><a href="#"> <i class="fa fa-envelope"></i> </a></li>
-                                    <li><a href="#"> <i class="fa fa-twitter"></i> </a></li>
-                                    <li><a href="#"> <i class="fa fa-linkedin-square"></i> </a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="moderator_name">
-                            <h3>Jack Piller</h3>
-                            <p>Photographer at COk</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xl-3 col-md-3">
-                    <div class="single_modarators">
-                        <div class="moderator_thumb">
-                            <img src="img/team/2.png" alt="">
-                            <div class="author_links">
-                                <ul>
-                                    <li><a href="#"> <i class="fa fa-envelope"></i> </a></li>
-                                    <li><a href="#"> <i class="fa fa-twitter"></i> </a></li>
-                                    <li><a href="#"> <i class="fa fa-linkedin-square"></i> </a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="moderator_name">
-                            <h3>Macau Wilium</h3>
-                            <p>Photographer at COk</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xl-3 col-md-3">
-                    <div class="single_modarators">
-                        <div class="moderator_thumb">
-                            <img src="img/team/3.png" alt="">
-                            <div class="author_links">
-                                <ul>
-                                    <li><a href="#"> <i class="fa fa-envelope"></i> </a></li>
-                                    <li><a href="#"> <i class="fa fa-twitter"></i> </a></li>
-                                    <li><a href="#"> <i class="fa fa-linkedin-square"></i> </a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="moderator_name">
-                            <h3>Ander Doe</h3>
-                            <p>Photographer at COk</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xl-3 col-md-3">
-                    <div class="single_modarators">
-                        <div class="moderator_thumb">
-                            <img src="img/team/4.png" alt="">
-                            <div class="author_links">
-                                <ul>
-                                    <li><a href="#"> <i class="fa fa-envelope"></i> </a></li>
-                                    <li><a href="#"> <i class="fa fa-twitter"></i> </a></li>
-                                    <li><a href="#"> <i class="fa fa-linkedin-square"></i> </a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="moderator_name">
-                            <h3>Clurk Amila</h3>
-                            <p>Photographer at COk</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- subscribe_newsletter_start -->
-    <div class="subscribe_newsletter">
-        <div class="container">
-            <div class="black_bg">
-                <div class="row">
-                    <div class="col-xl-6">
-                        <div class="newsletter_text">
-                            <h3>
-                                Subscribe newsletter
-                            </h3>
-                            <p>Get updates to our newsletter and new articles</p>
-                        </div>
-                    </div>
-                    <div class="col-xl-6">
-                        <div class="newsform">
-                            <form action="#">
-                                <input type="email" placeholder="Enter Your Email">
-                                <button type="submit">sign up</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- subscribe_newsletter_end -->
-
-    <!-- instagram_media_area_start -->
-    <div class="instagram_media_area">
-        <div class="container">
-            <div class="row">
-                <div class="col-xl-12">
-                    <div class="instagram_title text-center">
-                        <i class="fa fa-instagram"></i>
-                        <h3>@photomedia</h3>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xl-3 col-md-3">
-                    <div class="single_instragram">
-                        <img src="img/instagram/1.png" alt="">
-                    </div>
-                </div>
-                <div class="col-xl-3 col-md-3">
-                    <div class="single_instragram">
-                        <img src="img/instagram/2.png" alt="">
-                    </div>
-                </div>
-                <div class="col-xl-3 col-md-3">
-                    <div class="single_instragram">
-                        <img src="img/instagram/3.png" alt="">
-                    </div>
-                </div>
-                <div class="col-xl-3 col-md-3">
-                    <div class="single_instragram">
-                        <img src="img/instagram/4.png" alt="">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- instagram_media_area_end -->
 
     <!-- footer_start -->
     <footer class="footer">
@@ -299,6 +41,8 @@
                 <div class="row">
                     <div class="col-xl-12">
                         <div class="footer_info text-center">
+                            <div class="footer_logo text-center">
+                            </div>
                         </div>
                     </div>
                 </div>
