ac4a31f5869e28e5652710d2722b714afa2c063d
boomsocool <56288435+boomsocool@users.noreply.github.com>
2019-12-07 20:18:00 +0700

    มาล้ะ ของจริง
    ฟหก

---
+++ menuss.php
@@ -0,0 +1,67 @@
+        <div class="header-area ">
+            <div class="header_top">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-xl-4 col-md-4 d-none d-md-block">
+                            <div class="header_links ">
+                                <ul>
+                                    <li><a href="https://www.facebook.com/a.101111000110001011011100010101011001010"> <i class="fa fa-facebook"></i> </a></li>
+                                    <li><a class="twiter" href="#"> <i class="fa fa-twitter"></i> </a></li>
+                                    <li><a class="insta" href="https://www.instagram.com/"> <i class="fa fa-instagram"></i> </a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-xl-4 col-md-4">
+                            <div class="logo">
+                                <a href="index.php">
+                                    <img src="img/logos.png">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-xl-4 col-md-4 d-none d-md-block">
+                            <div class="login_resiter">
+                                <p><a href="logout.php" class="genric-btn danger-border circle"><i class="flaticon-user"></i>logout</a></p>
+                            </div>
+                            <div class="login_resiter" style="position:absolute">
+                            	<p style="color:#06F;">asdasd</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="sticky-header" class="main-header-area white-bg">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-xl-7 col-lg-7">
+                            <div class="main-menu  d-none d-lg-block">
+                                <nav>
+	<ul id="navigation">
+		<li><a href="indexs.php">Home</a></li>
+		<li><a href="category.php">Recipe</a></li>
+        <li><a href="category.php">Order List</a></li>
+		<li><a href="contact.php">Contact</a></li>
+		</ul>
+</nav>
+                            </div>
+                        </div>
+                        <div class="col-xl-5 col-lg-5">
+                            <div class="get_serch">
+                                <a id="search_1" href="javascript:void(0)"><i class="ti-search"></i></a>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="mobile_menu d-block d-lg-none"></div>
+                        </div>
+                    </div>
+                    <div class="search_input" id="search_input_box">
+                            <div class="container ">
+                                <form class="d-flex justify-content-between search-inner">
+                                    <input type="text" class="form-control" id="search_input" placeholder="Search Here">
+                                    <button type="submit" class="btn"></button>
+                                    <span class="ti-close" id="close_search" title="Close Search"></span>
+                                </form>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div>
