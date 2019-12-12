626be1d3082d9563fef98177ebb647eeccff35df
boomsocool <56288435+boomsocool@users.noreply.github.com>
2019-12-12 20:12:30 +0700

    ทดสอบ
    ฟหก

---
+++ rez8s.php
@@ -0,0 +1,13 @@
+<?
+	include("connect.php");
+	$add1 = $_POST['order1'];
+	$add2 = $_POST['order2'];
+	$add3 = $_POST['order3'];
+	$add4 = $_POST['order4'];
+	$sql = "INSERT INTO `1.s`(`Name`, `Menu`, `Type`, `ETC`) ";
+	$sql .= " VALUES ('$add1','$add2','$add3','$add4')";
+	mysql_query($sql) or die();
+?>
+<script>
+	window.location = "rez8.php";
+</script>
